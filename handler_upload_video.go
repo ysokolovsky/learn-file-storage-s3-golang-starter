02760c5bf5f21c04c0b632279e001c71a7111151
ysokolovsky <yaroslav.sokolovsky@gmail.com>
2025-12-30 22:13:57 +0200

    handler_upload_video

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,7 +1,101 @@
 package main
 
 import (
+	"context"
+	"crypto/rand"
+	"encoding/base64"
+	"errors"
+	"io"
+	"mime"
 	"net/http"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	"github.com/google/uuid"
 )
 
-func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {}
+func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {
+	r.Body = http.MaxBytesReader(w, r.Body, 1<<30)
+	defer r.Body.Close()
+
+	videoIDString := r.PathValue("videoID")
+	videoID, err := uuid.Parse(videoIDString)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid ID", err)
+		return
+	}
+
+	token, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't find JWT", err)
+		return
+	}
+
+	userID, err := auth.ValidateJWT(token, cfg.jwtSecret)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't validate JWT", err)
+		return
+	}
+
+	videoMetadata, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to get video metadata", err)
+		return
+	}
+
+	if videoMetadata.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "unauthorized", errors.New("unauthorized"))
+		return
+	}
+
+	const maxMemory = 1 << 30
+
+	r.ParseMultipartForm(maxMemory)
+
+	file, header, err := r.FormFile("video")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
+
+	mediaType, _, err := mime.ParseMediaType(header.Header.Get("Content-Type"))
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+
+	if mediaType != "video/mp4" {
+		respondWithError(w, http.StatusBadRequest, "file is not a video", err)
+		return
+	}
+
+	tempFile, _ := os.CreateTemp("", "tubely-upload.mp4")
+	defer os.Remove(tempFile.Name())
+	defer tempFile.Close()
+
+	io.Copy(tempFile, file)
+
+	tempFile.Seek(0, io.SeekStart)
+
+	key := make([]byte, 32)
+	rand.Read(key)
+	vkey := base64.RawURLEncoding.EncodeToString(key)
+
+	poi := s3.PutObjectInput{
+		Bucket:      &cfg.s3Bucket,
+		Key:         &vkey,
+		Body:        tempFile,
+		ContentType: &mediaType,
+	}
+
+	_, err = cfg.s3Client.PutObject(context.TODO(), &poi)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "s3 upload error", err)
+	}
+
+	videoURL := "https://tubely-6661337.s3.eu-north-1.amazonaws.com/" + vkey
+	videoMetadata.VideoURL = &videoURL
+	cfg.db.UpdateVideo(videoMetadata)
+}
--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/database"
 
 	"github.com/joho/godotenv"
@@ -21,11 +24,7 @@ type apiConfig struct {
 	s3Region         string
 	s3CfDistribution string
 	port             string
-}
-
-type thumbnail struct {
-	data      []byte
-	mediaType string
+	s3Client         *s3.Client
 }
 
 func main() {
@@ -81,6 +80,13 @@ func main() {
 		log.Fatal("PORT environment variable is not set")
 	}
 
+	awsCfg, err := config.LoadDefaultConfig(context.Background(), config.WithRegion(s3Region))
+	if err != nil {
+		log.Fatal("failed to get aws config")
+	}
+
+	s3Client := s3.NewFromConfig(awsCfg)
+
 	cfg := apiConfig{
 		db:               db,
 		jwtSecret:        jwtSecret,
@@ -91,6 +97,7 @@ func main() {
 		s3Region:         s3Region,
 		s3CfDistribution: s3CfDistribution,
 		port:             port,
+		s3Client:         s3Client,
 	}
 
 	err = cfg.ensureAssetsDir()
